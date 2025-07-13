2f13aaae1c9c8001c9fa591d56d15b1ad129711a
Alijassim18 <iceali927@gmail.com>
2025-07-13 10:48:58 +0300

    i create the array

---
+++ scripts/app.js
@@ -0,0 +1,12 @@
+const questions=[
+    {question:'komark',type:'text',answer:'dk',audio:'#',image:''},
+    {question:'',type:'',answer:'',audio:'',image:''} ,
+    {question:'',type:'',answer:'',audio:'',image:''},
+    {question:'',type:'',answer:'',audio:'',image:''},
+    {question:'',type:'',answer:'',audio:'',image:''},
+]
+
+
+
+const questionElm=document.getElementById('question')
+
